112dd6f3dfbf1e5a208e8c65cc45a145724e294b
jones <35585020+fleeced@users.noreply.github.com>
2020-11-07 11:38:21 -0700

    Updated code before adding Firebase Realtime Database

--- app/src/main/java/com/example/phosho/SearchActivity.java
+++ app/src/main/java/com/example/phosho/SearchActivity.java
@@ -1,14 +1,29 @@
 package com.example.phosho;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private EditText mSearchField;
+    private ImageButton mSearchBtn;
+
+    private RecyclerView mResultList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        mSearchField = (EditText) findViewById(R.id.search_field);
+        mSearchBtn = (ImageButton) findViewById(R.id.button_search);
+
+        mResultList = (RecyclerView) findViewById(R.id.list_results);
+
+
     }
 }
