731ef7dc66dd92302c88751728954fb4785db5f6
jones <35585020+fleeced@users.noreply.github.com>
2020-11-08 14:00:17 -0700

    SearchActivity.java has code that doesnt work

--- app/src/main/java/com/example/phosho/SearchActivity.java
+++ app/src/main/java/com/example/phosho/SearchActivity.java
@@ -1,11 +1,26 @@
 package com.example.phosho;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.data.model.User;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -14,16 +29,77 @@ public class SearchActivity extends AppCompatActivity {
 
     private RecyclerView mResultList;
 
+    private DatabaseReference mUserDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        mUserDatabase = FirebaseDatabase.getInstance().getReference("Users");
+
         mSearchField = (EditText) findViewById(R.id.search_field);
         mSearchBtn = (ImageButton) findViewById(R.id.button_search);
 
         mResultList = (RecyclerView) findViewById(R.id.list_results);
+        mResultList.setHasFixedSize(true);
+        mResultList.setLayoutManager(new LinearLayoutManager(this));
+
+        mSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String searchText = mSearchField.getText().toString();
+
+                //firebaseUserSearch(searchText);
+            }
+        });
+    }
+
+    private void firebaseUserSearch(String searchText) {
+
+        Toast.makeText(SearchActivity.this, "Clicked button", Toast.LENGTH_LONG).show();
+
+        Query firebaseSearchQuery = mUserDatabase.orderByChild("name").startAt(searchText).endAt(searchText + "\uf8ff");
+
+        /*FirebaseRecyclerAdapter<Users, UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Users, UsersViewHolder>(
+                Users.class,
+                R.layout.list_layout,
+                UsersViewHolder.class,
+                mUserDatabase
+        ) {
+            @Override
+            protected void populateViewHolder(UsersViewHolder viewHolder, Users model, int position) {
+                viewHolder.setDetails(model.getName(), model. getStatus(), model.getImage());
+            }
+        };
 
 
+
+        mResultList.setAdapter(firebaseRecyclerAdapter);
+        
+         */
+    }
+
+    public class UsersViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public UsersViewHolder(View itemView) {
+            super(itemView);
+
+            mView = itemView;
+        }
+
+        public void setDetails(String name, String status, String image) {
+            TextView user_name = (TextView) mView.findViewById(R.id.username);
+            TextView user_description = (TextView) mView.findViewById(R.id.search_description);
+            ImageView user_image = (ImageView) mView.findViewById(R.id.imageview_profile);
+
+            user_name.setText(name);
+            user_description.setText(status);
+
+            Glide.with(getApplicationContext()).load(image).into(user_image);
+        }
     }
 }
